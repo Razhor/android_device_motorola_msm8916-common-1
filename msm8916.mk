3227c177ef496a20a37a691d4cc4b97646d192ca
Ekko <erikofarias@gmail.com>
2018-12-04 05:11:02 -0300

    msm8196: comment thermal service

--- msm8916.mk
+++ msm8916.mk
@@ -233,8 +233,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-impl
+#    android.hardware.thermal@1.0-service
 
 # Power
 PRODUCT_PACKAGES += \
