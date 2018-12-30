2e371b0ae9b953afd5fe3a04a6407942a8041542
srfarias <erikofarias@gmail.com>
2018-12-30 15:01:12 -0300

    msm8916: correctly binderized bluetooth

--- msm8916.mk
+++ msm8916.mk
@@ -84,7 +84,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0 \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
 # Camera
