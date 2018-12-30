4607127feddc497dcb3c94fc7cb28e13adb60cc7
Razhor <zamorano0695@gmail.com>
2018-12-30 09:34:49 -0700

    harpia: setup gcc7 kernel toolchain

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,9 +45,15 @@ TARGET_CPU_VARIANT := cortex-a53
 # Binder API version
 TARGET_USES_64_BIT_BINDER := true
 
+# Kernel Toolchain
+ifneq ($(wildcard $(shell pwd)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-7.3),)
+  KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-7.3/bin
+  KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+endif
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M androidboot.bootdevice=7824900.sdhci utags.blkdev=/dev/block/bootdevice/by-name/utags utags.backup=/dev/block/bootdevice/by-name/utagsBackup movablecore=160M
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
@@ -55,9 +61,10 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8916
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 TARGET_EXFAT_DRIVER := sdfat
+BOARD_KERNEL_CMDLINE += pm.sleep_mode=1
+
 TARGET_USES_MKE2FS := true
 
 # Audio
